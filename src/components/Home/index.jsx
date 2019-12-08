f73772b4cbdcaca487513a1119fb6f70b008c9b9
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-12-08 16:22:29 +0300

    Edit second slide

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,11 +3,12 @@ import "./Home.scss";
 import "./Home-media.scss";
 import ReactHtmlParser from "react-html-parser";
 import classNames from "classnames";
-import { Element } from 'react-scroll'
+import { Element } from 'react-scroll';
 
-import first_slide_1 from '../../assets/images/slide_1/slide_1_img_1.png';
-import first_slide_2 from '../../assets/images/slide_1/slide_1_img_2.png';
+
 import image_1 from '../../assets/images/home/image_1.jpg';
+import third_slide_image from '../../assets/images/home/third_slide_image.png';
+
 
 export default class Home extends React.Component {
     constructor(props) {
@@ -83,16 +84,17 @@ export default class Home extends React.Component {
         });
         return (
             <div className="Home">
-                   <div className="first-slide-container">
+                   <section className="first-slide-container">
                        <div 
                             className={mobileClasses}
                             ref={this.mobileWow}
                             className="text-block-blur"
                         >
                             <div className="text-block">
-                             <h1 className="header-first-slide">{this.props.text.Header}</h1>
+                             <h1 className="header-first-slide">{this.props.text.HeaderFirstSlide}</h1>
                              <hr class="line-first-slide"></hr>
-                             
+                            <p>{this.props.text.TextFirstSlide_1}</p>
+                            <p>{this.props.text.TextFirstSlide_2}</p>
                             </div>
                        </div>
                         <div>
@@ -104,43 +106,41 @@ export default class Home extends React.Component {
                                 alt="Mobile background"
                             />
                         </div>
-                    </div>
-
-
-                <div className="container" id="toGetStarted">
-                    <section
-                        className="content content-ourApp"
-                        id={this.sections.toGetStarted.id}
-                        ref={this.sections.toGetStarted.ref}
-                    >
-                        <div className="first-slide">
-                            <div className="img">
-                                <img
-                                    src={image_1}
-                                    className={mobileClasses}
-                                    ref={this.mobileWow}
-                                    alt="EasyLang app"
-                                />
-                            </div>
-                         
-                        </div>
+                         <div className="red-block"></div>
                     </section>
-                    <Element id='example-destination' name='example-destination'>
-                    <div className="first-slide">
-                            <div className="img">
-                                <img
-                                    src={image_1}
-                                    className={mobileClasses}
-                                    ref={this.mobileWow}
-                                    alt="EasyLang app"
+
+                    <section className="second-slide-container">
+                         <div className="second-red-block">
+                                <h1 className="header-second-slide">{this.props.text.HeaderSecondSlide}</h1>
+                         </div>
+                         <div className="second-text-block">
+                            <p>{this.props.text.TextSecondSlide_1}</p>
+                            <p>{this.props.text.TextSecondSlide_2}</p>
+                            <p>{this.props.text.TextSecondSlide_2}</p>
+                            <p>{this.props.text.TextSecondSlide_4}</p>
+                            <p>{this.props.text.TextSecondSlide_5}</p>
+                         </div>
+                    </section>
+
+
+                    <section className="third-slide-container">
+                         <div className="third-image-block">
+                                <img className="third-slide-image" 
+                                src={third_slide_image}
                                 />
-                            </div>
-                         
-                        </div>
-      </Element>
+
+                         </div>
+                         <div className="third-text-block">
+                            <h1 className="header-second-slide">{this.props.text.HeaderThirdSlide}</h1>
+                            <p>{this.props.text.TextSecondSlide_1}</p>
+                            <p>{this.props.text.TextSecondSlide_2}</p>
+                            <p>{this.props.text.TextSecondSlide_2}</p>
+                            <p>{this.props.text.TextSecondSlide_4}</p>
+                            <p>{this.props.text.TextSecondSlide_5}</p>
+                         </div>
+                    </section>
 
 
-                </div>
             </div>
         );
     }
